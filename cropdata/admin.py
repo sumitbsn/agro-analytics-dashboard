7e1e370ebabc0a8588c8de3fdf7a90ca0e477ee8
sumitbsn <sumitbsn@gmail.com>
2018-02-16 19:45:04 +0530

    implemented comment in admin.py

--- cropdata/admin.py
+++ cropdata/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Pressure, Cropdata, Blog
+from .models import Pressure, Cropdata, Blog, Comment
 
 
 #myModels = [models.Project, models.Client, models.About]  # iterable list
 #admin.site.register(myModels)
 
 admin.site.register(Blog)
-# admin.site.register(Comment)
+admin.site.register(Comment)
 # admin.site.register(Post)
